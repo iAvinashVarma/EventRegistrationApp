b07e333df5ad7df6b38cf4a6262c5a133c539219
Avinash Varma <avinashvarma@live.com>
2019-05-03 11:42:19 +0800

    Replace href using location service.

---
+++ app/js/controllers/mainMenuCtrl.js
@@ -0,0 +1,17 @@
+'use strict';
+
+eventsApp.controller('mainMenuCtrl',
+    function mainMenuCtrl($scope, $location, $log){
+        $log.info('absUrl: ' + $location.absUrl());
+        $log.info('protocols: ' + $location.protocol());
+        $log.info('port: ' + $location.port());
+        $log.info('host: ' + $location.host());
+        $log.info('path: ' + $location.path());
+        $log.info('search: ' + $location.search());
+        $log.info('hash: ' + $location.hash());
+        $log.info('url: ' + $location.url());
+        $scope.createEvent = function(){
+            $location.url('/newEvent');
+        }
+    }
+);
