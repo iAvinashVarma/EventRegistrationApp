2769728f727d65f8080f4be247c13b1052dfc78f
Avinash Varma <avinashvarma@live.com>
2019-05-03 19:02:30 +0800

    Isolating event scope using equal sign.

--- app/js/controllers/mainMenuCtrl.js
+++ app/js/controllers/mainMenuCtrl.js
@@ -1,7 +1,14 @@
 'use strict';
 
 eventsApp.controller('mainMenuCtrl',
-    function mainMenuCtrl($scope, $location){
+    function mainMenuCtrl($scope, $location, $window){
+        $window.onload = function() {
+            $(".navbar a").on("click", function(){
+                $(".navbar").find(".active").removeClass("active");
+                $(this).parent().addClass("active");
+            });
+        };
+
         $scope.createEvent = function(){
             $location.replace();
             $location.url('/newEvent');
--- app/js/directives/calculateHike.js
+++ app/js/directives/calculateHike.js
@@ -3,9 +3,9 @@
 eventsApp.directive('calculateHike', function($compile){
     return {
         restrict: 'E',
-        template: "<div>Old Salary: <input type='number' ng-model='oldSalary' placeholder='Old Salary'/><br/>{{ oldSalary }}<br/>New Salary: <input type='number' ng-model='newSalary' placeholder='New Salary'/><br/>{{ newSalary }}<hr/>Hike Percentage: <p ng-if='newSalary && oldSalary'>{{ (newSalary - oldSalary)/oldSalary * 100 }}</p></div>";
+        template: "<div>Old Salary: <input type='number' ng-model='oldSalary' placeholder='Old Salary'/><br/>{{ oldSalary }}<br/>New Salary: <input type='number' ng-model='newSalary' placeholder='New Salary'/><br/>{{ newSalary }}<hr/>Hike Percentage: <p ng-if='newSalary && oldSalary'>{{ (newSalary - oldSalary)/oldSalary * 100 }}</p></div>",
         scope: {
-
+            event: '=event'
         }
     };
 });
