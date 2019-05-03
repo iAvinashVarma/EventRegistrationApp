bd04cd9bfc40431d5fe81b45001cd3ff6382d05a
Avinash Varma <avinashvarma@live.com>
2019-05-03 18:40:54 +0800

    Replace directive link with directive template.

--- app/js/directives/calculateHike.js
+++ app/js/directives/calculateHike.js
@@ -3,10 +3,7 @@
 eventsApp.directive('calculateHike', function($compile){
     return {
         restrict: 'E',
-        link: function(scope, element, attrs, controller){
-            var markup = "<div>Old Salary: <input type='number' ng-model='oldSalary' placeholder='Old Salary'/><br/>{{ oldSalary }}<br/>New Salary: <input type='number' ng-model='newSalary' placeholder='New Salary'/><br/>{{ newSalary }}<hr/>Hike Percentage: <p ng-if='newSalary && oldSalary'>{{ (newSalary - oldSalary)/oldSalary * 100 }}</p></div>";
-            angular.element(element).html($compile(markup)(scope));
-        },
+        template: "<div>Old Salary: <input type='number' ng-model='oldSalary' placeholder='Old Salary'/><br/>{{ oldSalary }}<br/>New Salary: <input type='number' ng-model='newSalary' placeholder='New Salary'/><br/>{{ newSalary }}<hr/>Hike Percentage: <p ng-if='newSalary && oldSalary'>{{ (newSalary - oldSalary)/oldSalary * 100 }}</p></div>";
         scope: {
 
         }
