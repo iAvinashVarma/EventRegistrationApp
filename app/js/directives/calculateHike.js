5ccf5b8574f6c9087f6ec317a04d7fefca9256b4
Avinash Varma <avinashvarma@live.com>
2019-05-03 19:18:36 +0800

    Add isolate scope binding.

--- app/js/directives/calculateHike.js
+++ app/js/directives/calculateHike.js
@@ -5,7 +5,6 @@ eventsApp.directive('calculateHike', function($compile){
         restrict: 'E',
         template: "<div>Old Salary: <input type='number' ng-model='oldSalary' placeholder='Old Salary'/><br/>{{ oldSalary }}<br/>New Salary: <input type='number' ng-model='newSalary' placeholder='New Salary'/><br/>{{ newSalary }}<hr/>Hike Percentage: <p ng-if='newSalary && oldSalary'>{{ (newSalary - oldSalary)/oldSalary * 100 }}</p></div>",
         scope: {
-            event: '=event'
         }
     };
 });
