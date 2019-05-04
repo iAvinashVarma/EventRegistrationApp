6b24fdf97251764d8a5f765d953aacbfb06eba96
Avinash Varma <avinashvarma@live.com>
2019-05-04 19:29:40 +0800

    Use compile property in directive to transform the DOM.

--- app/js/directives/repeatx.js
+++ app/js/directives/repeatx.js
@@ -2,9 +2,9 @@
 
 eventsApp.directive('repeatX', function($compile){
     return {
-        link: function(scope, element, attrs, controller){
-            for(var i = 0; i < Number(attrs.repeatX) - 1; i++){
-                element.after($compile(element.clone().attr('repeat-x', 0))(scope));
+        compile: function(element, attributes){
+            for(var i = 0; i < Number(attributes.repeatX) - 1; i++){
+                element.after(element.clone().attr('repeat-x', 0));
             }
         }
     }
