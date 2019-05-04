d41c9a3c822036247157448449a08e9195532a71
Avinash Varma <avinashvarma@live.com>
2019-05-04 19:37:23 +0800

    Add link function within compile.

--- app/js/directives/repeatx.js
+++ app/js/directives/repeatx.js
@@ -1,11 +1,20 @@
 'use strict';
 
-eventsApp.directive('repeatX', function($compile){
+eventsApp.directive('repeatX', function(){
     return {
         compile: function(element, attributes){
             for(var i = 0; i < Number(attributes.repeatX) - 1; i++){
                 element.after(element.clone().attr('repeat-x', 0));
             }
+            return function(scope, element, attributes, controller){
+                attributes.$observe('text', function(newValue){
+                    if(newValue.length%2 == 0){
+                        element.css('color', 'red');
+                    } else {
+                        element.css('color', 'black');
+                    }
+                });
+            }
         }
-    }
+    };
 });
