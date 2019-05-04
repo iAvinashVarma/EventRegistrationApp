51ee95a0684a16c76c96a1186c9877bbc60fb03f
Avinash Varma <avinashvarma@live.com>
2019-05-04 15:38:19 +0800

    Share directive controllers via require and link property.

--- app/js/directives/greeting.js
+++ app/js/directives/greeting.js
@@ -1,18 +1,37 @@
 'use strict';
 
-eventsApp.directive('greeting', function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
-        controller: '@',
-        name: 'ctrl'
-    }
-});
-
-eventsApp.controller('greetingCtrl', 
-    function greetingCtrl($scope){
-        $scope.sayHello = function(){
-            alert('Hello');
+eventsApp
+    .directive('greeting', function(){
+        return {
+            restrict: 'E',
+            replace: true,
+            template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
+            controller: function($scope){
+                var greetings = ['Hello'];
+                $scope.sayHello = function(){
+                    alert(greetings.join());
+                }
+                this.addGreeting = function(greeting){
+                    greetings.push(greeting);
+                }
+            }
         }
-});
+    })
+    .directive('finnish', function(){
+        return {
+            restrict: 'A',
+            require: 'greeting',
+            link: function(scope, element, attrs, controller){
+                controller.addGreeting('hei');
+            }
+        }
+    })
+    .directive('hindi', function(){
+        return {
+            restrict: 'A',
+            require: 'greeting',
+            link: function(scope, element, attrs, controller){
+                controller.addGreeting('Namaste');
+            }
+        }
+    })
