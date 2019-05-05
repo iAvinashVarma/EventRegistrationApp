7285c70e88b987299808a61816d76ae7ad07e50b
Avinash Varma <avinashvarma@live.com>
2019-05-05 09:03:34 +0800

    Test AJAX services.

---
+++ test/unit/eventDataSpec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('eventData', function(){
+    beforeEach(module('eventsApp'));
+
+    it('Should issue a GET request to /data/event/11 when getEvent is called and the id is 11', inject(function(eventData, $httpBackend){
+        $httpBackend.expectGET('/data/event/11');
+        $httpBackend.when('GET', '/data/event/11').respond({});
+        eventData.getEvent(11);
+        $httpBackend.flush();
+
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    }))
+});
