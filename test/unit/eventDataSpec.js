a3ff9f896fb81468fb13e769192519dcc5b13d0a
Avinash Varma <avinashvarma@live.com>
2019-05-05 09:21:24 +0800

    Test AJAX services #2.

--- test/unit/eventDataSpec.js
+++ test/unit/eventDataSpec.js
@@ -12,4 +12,29 @@ describe('eventData', function(){
         $httpBackend.verifyNoOutstandingExpectation();
         $httpBackend.verifyNoOutstandingRequest();
     }))
+
+    it('Should return correct data when getEvent is called', inject(function(eventData, $httpBackend){
+        $httpBackend.when('GET', '/data/event/11').respond({name: 'My Event'});
+        var event = eventData.getEvent(11);
+        $httpBackend.flush();
+
+        expect(event.name).toBe('My Event');
+    }))
+
+    it('Should set the id to 999 when save is called.', inject(function(eventData, $httpBackend){
+        $httpBackend.when('POST', '/data/event/999').respond({});
+        var event = {name: 'My Event', id: 999};
+        eventData.save(event);
+        $httpBackend.flush();
+
+        expect(event.id).toBe(999);
+    }))
+
+    it('Should issue a GET request to data/event when getAllEvents is called', inject(function(eventData, $httpBackend){
+        $httpBackend.when('GET', '/data/event').respond([{name: 'My Event'}]);
+        var events = eventData.getAllEvents();
+        $httpBackend.flush();
+
+        expect(events[0].name).toBe('My Event');
+    }))
 });
